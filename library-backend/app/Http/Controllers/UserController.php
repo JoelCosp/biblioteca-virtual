ab44bad2c8c2c1cdcf79a7189b573500689a3e2b
JoelVRV <joelc456789@gmail.com>
2025-02-04 12:27:08 +0100

    BACKEND REGISTRO/LOGIN CREADO CON JWT
    - Se ha instalado todo lo del JWT, se han creado las funciones de REGISTER y LOGIN y funciona bien.

--- library-backend/app/Http/Controllers/UserController.php
+++ library-backend/app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash; // Para usar el BCRYPT
 use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTExceptions;
 
 class UserController extends Controller
 {
@@ -24,7 +26,7 @@ class UserController extends Controller
             $user = User::Create([
                 'name' => $request->name,
                 'email' => $request->email,
-                'password' => password_hash($request->email, PASSWORD_BCRYPT)
+                'password' => Hash::make($request->password)
             ]);
 
             $response['status'] = 1;
@@ -32,6 +34,40 @@ class UserController extends Controller
             $response['code'] = 200;
         }
 
+        return response()->json($response);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        try
+        {
+            if(!JWTAuth::attempt($credentials))
+            {
+                $response['status'] = 0;
+                $response['code'] = 401;
+                $response['data'] = null;
+                $response['message'] = 'Email or Password is incorrect';
+                return response()->json($response);
+            }
+        }
+        catch(JWTException $e)
+        {
+            $response['code'] = 500;
+            $response['data'] = null;
+            $response['message'] = 'Could Not Create Token';
+            return response()->json($response);
+        }
+        $user = auth()->user();
+        $data['token'] = auth()->claims([
+            'user_id' => $user->id,
+            'email' => $user->email
+        ])->attempt($credentials);
+
+        $response['status'] = 1;
+        $response['code'] = 200;
+        $response['data'] = $data;
+        $response['message'] = 'Login Successfully';
         return response()->json($response);
     }
 }
--- library-backend/routes/api.php
+++ library-backend/routes/api.php
@@ -12,7 +12,8 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 // Login
-Route::post('/user', [UserController::class, 'register']); // Crear usuario
+Route::post('/register', [UserController::class, 'register']); // Crear usuario
+Route::post('/login', [UserController::class, 'login']); // Crear usuario
 
 
 
